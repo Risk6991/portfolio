37084d1164214d4ec13d80dc045152ecff815845
Risk6991 <hotmail422@gmail.com>
2020-09-25 17:30:51 +0900

    event発火せず動かない

--- js/main.js
+++ js/main.js
@@ -1,26 +1,32 @@
 "use strict";
 
-const clickTrigger = document.getElementById("colorChange");
-clickTrigger.addEventListener("click", function () {
-    let headTitle = document.getElementById("headTitle");
-    headTitle.classList.add("class", "text-color");
-});
+addEventListener()
+window.onload = function () {
+    let paymentSelect = document.getElementById("paymentSelect");
+    paymentSelect.onchange = function () {
+        //ドロップダウンの値を取得する
+        let paymentSelectStr = paymentSelect.nodeValue;
+        let paymentSelectInt = Number(paymentSelectStr);
 
-
-const defaultTrigger = document.getElementById("colorDefault");
-defaultTrigger.addEventListener("click", function () {
-    let headTitle = document.getElementById("headTitle");
-    headTitle.classList.remove("class");
-});
-
-
-
-document.getElementById("inputTrigger").onclick = function () {
-    let freewordInput = document.getElementById("freewordInput");
-    let freewordInputValue = freewordInput.value;
-    console.log(freewordInput);
-    console.log(freewordInputValue);
-    let showInput = document.getElementById("showInput");
-    console.log(showInput);
-    showInput.textContent = `入力された値：${freewordInputValue}`;
+        let paymentSelectResultStr = "手数料:　";
+        switch (paymentSelectInt) {
+            case 1:
+                paymentSelectResultStr += "432円";
+                break;
+            case 2:
+                paymentSelectResultStr += "80円";
+                break;
+            case 3:
+                paymentSelectResultStr += "0円";
+                break;
+            case 4:
+                paymentSelectResultStr += "300円";
+                break;
+            default:
+                paymentSelectResultStr += "";
+                break;
+        }
+        let paymentSelectResult = document.getElementById("paymentSelectResult");
+        paymentSelectResult.textContent = paymentSelectResultStr;
+    };
 };
