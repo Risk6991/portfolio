a41d9b3bb790c29b1cd49dd80d20406757666147
Risk6991 <hotmail422@gmail.com>
2020-09-25 21:31:38 +0900

    setting-photo-swipe

--- js/main.js
+++ js/main.js
@@ -1,26 +1,14 @@
 "use strict";
 
-const clickTrigger = document.getElementById("colorChange");
-clickTrigger.addEventListener("click", function () {
-    let headTitle = document.getElementById("headTitle");
-    headTitle.classList.add("class", "text-color");
+const mySwiper = new Swiper('.swiper-container', {
+    speed: 600,
+    navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+		el: '.swiper-pagination',
+		type: 'bullets',
+		clickable: true
+	}
 });
-
-
-const defaultTrigger = document.getElementById("colorDefault");
-defaultTrigger.addEventListener("click", function () {
-    let headTitle = document.getElementById("headTitle");
-    headTitle.classList.remove("class");
-});
-
-
-
-document.getElementById("inputTrigger").onclick = function () {
-    let freewordInput = document.getElementById("freewordInput");
-    let freewordInputValue = freewordInput.value;
-    console.log(freewordInput);
-    console.log(freewordInputValue);
-    let showInput = document.getElementById("showInput");
-    console.log(showInput);
-    showInput.textContent = `入力された値：${freewordInputValue}`;
-};
