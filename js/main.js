507336d91a5e7218901b00304a26cb70dcf659ee
Risk6991 <hotmail422@gmail.com>
2020-10-15 16:24:04 +0900

    test-scrollReveal

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 "use strict";
 
-const mySwiper = new Swiper('.swiper-container', {
+const mySwiper = new Swiper(".swiper-container", {
     speed: 600,
     navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev'
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     pagination: {
-		el: '.swiper-pagination',
-		type: 'bullets',
-		clickable: true
-	}
+        el: ".swiper-pagination",
+        type: "bullets",
+        clickable: true,
+    },
 });
+
+const revealSetting = {
+    distance: "300px",
+    origin: "left",
+    viewFactor: "0.5",
+};
+
+ScrollReveal().reveal(".animate-right", revealSetting);
+ScrollReveal().reveal(".headline", revealSetting);
